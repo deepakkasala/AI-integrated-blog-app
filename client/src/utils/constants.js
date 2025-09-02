5327da23c738cb2b44c7879adab51c925af0277c
Deepak Kasala <deepakkasala14@gmail.com>
2025-09-02 22:36:27 +0530

    Cors issue fixed

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,4 @@
 const PORT = 3020;
-export const BASE_URL = `http://localhost:${PORT}`;
+// export const BASE_URL = `http://localhost:${PORT}`;
+// src/utils/Constants.js
+export const BASE_URL = import.meta.env.VITE_API_URL;
