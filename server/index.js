34ac66a661145313d9f2ee37640415c79b924c8d
Deepak Kasala <deepakkasala14@gmail.com>
2025-09-02 18:24:59 +0530

    updated code on cors issue

--- server/index.js
+++ server/index.js
@@ -8,7 +8,13 @@ const commentRoutes = require("./routes/comment");
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "*", // or list your frontend domain here
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 app.use("/uploads", express.static("public/uploads"));
 
