54457afc98c776f3d1143f5d22cf9df2c4174397
Deepak Kasala <deepakkasala14@gmail.com>
2025-09-02 23:38:20 +0530

    .env files setup

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
-const PORT = 3020;
+// const PORT = 3020;
 
-export const BASE_URL = import.meta.env.BASE_URL;
-console.log(BASE_URL);
+export const BASE_URL = import.meta.env.VITE_API_URL;
+console.log("Backend API URL =>", BASE_URL);
